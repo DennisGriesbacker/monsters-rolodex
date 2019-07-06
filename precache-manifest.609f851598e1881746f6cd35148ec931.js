823a564d5789a6fd93ba8eaadcc0dd4cbbbb7f29
DGriesbach <43821099+DGriesbach@users.noreply.github.com>
2019-07-06 13:43:31 +0200

    Updates

---
+++ precache-manifest.609f851598e1881746f6cd35148ec931.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc921247d0ffc0073c0da23403159db0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "88c08e66a26fb623eecf",
+    "url": "/monsters-rolodex/static/css/main.a9e5fb52.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "88c08e66a26fb623eecf",
+    "url": "/monsters-rolodex/static/js/main.b36dc053.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
